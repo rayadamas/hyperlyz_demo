d5c7aa965697494855ded9cb9d50fd84a0ed2a7e
Valerie <valdaraandy@gmail.com>
2022-08-06 11:01:13 -0400

    chnaged header and added Dasher's art

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,40 @@
 import Link from "next/link";
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 
-export default function Header() {
+export const Header = () => {
+
    return (
-      <div className="fixed top-0 right-0">
-         <ConnectButton accountStatus="address" showBalance={false}  />
-      </div>
+     <div className="bg-black text-white flex flex-row justify-center">
+       <div className="fixed top-0 right-0">
+         <ConnectButton 
+           accountStatus="address" 
+           showBalance={false}
+         />
+       </div>
+       <div className="z-10 bg-black fixed bottom-24 sm:bottom-0 px-4 py-w border-2 border-solid border-white flex flex-row w-fit space-x-4">
+         <Link
+           href="/"
+         >
+           <a className="hover:text-[#f53bc3]">
+           HOME
+           </a>
+         </Link>
+         <Link
+           href="/mintViz"
+         >
+           <a className="hover:text-[#f53bc3]">
+             MINT
+           </a>
+         </Link>
+         <Link
+           href="/Gallery"
+         >
+           <a className="hover:text-[#f53bc3]">
+             GALLERY
+           </a>
+         </Link>
+       </div>
+     </div>
    )
-}
+ 
+ };
