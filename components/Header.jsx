a11e191135d21260bd0dd96cbbc67989263dd25a
Valerie <valdaraandy@gmail.com>
2022-08-06 13:15:52 -0400

    header is reverting

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,40 +1,10 @@
 import Link from "next/link";
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 
-export const Header = () => {
-
+export default function Header() {
    return (
-     <div className="bg-black text-white flex flex-row justify-center">
-       <div className="fixed top-0 right-0">
-         <ConnectButton 
-           accountStatus="address" 
-           showBalance={false}
-         />
-       </div>
-       <div className="z-10 bg-black fixed bottom-24 sm:bottom-0 px-4 py-w border-2 border-solid border-white flex flex-row w-fit space-x-4">
-         <Link
-           href="/"
-         >
-           <a className="hover:text-[#f53bc3]">
-           HOME
-           </a>
-         </Link>
-         <Link
-           href="/mintViz"
-         >
-           <a className="hover:text-[#f53bc3]">
-             MINT
-           </a>
-         </Link>
-         <Link
-           href="/Gallery"
-         >
-           <a className="hover:text-[#f53bc3]">
-             GALLERY
-           </a>
-         </Link>
-       </div>
-     </div>
+      <div className="fixed top-0 right-0">
+         <ConnectButton accountStatus="address" showBalance={false}  />
+      </div>
    )
- 
- };
+}
